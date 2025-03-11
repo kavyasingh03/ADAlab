7f5c1012d6ab7babf9d0fd116a0f73810617230c
kavyasingh03 <kavyasingh.cs23@bmsce.ac.in>
2025-03-11 09:51:06 +0530

    reverselinkedlist.c

---
+++ week 2/reverselinkedlist.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* current = head;
+    struct ListNode* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;  
+        current->next = prev;  
+        prev = current;    
+        current = next; 
+    }
+    return prev;
+}
+
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
